e46e915959cb2a2fc9ab74393a1e295818c7c262
Renzo <renzo.roca.m@gmail.com>
2021-01-09 14:29:13 +0100

    Iteration 1 done

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const path = require('path');
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(
-  `${app_name}:${path.basename(__filename).split('.')[0]}`
+    `${app_name}:${path.basename(__filename).split('.')[0]}`
 );
 
 const app = express();
 
 // require database configuration
 require('./configs/db.config');
+require('./bin/seeds')
 
 // Middleware Setup
 app.use(logger('dev'));
